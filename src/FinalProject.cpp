02f87e412d89f718dbbe8ac2b7a246ee5ed0ed05
gagl2155 <gagl2155@colorado.edu>
2015-04-28 07:46:30 -0600

    Adding some comments to main.cpp file

--- src/FinalProject.cpp
+++ src/FinalProject.cpp
@@ -5,7 +5,9 @@
 using namespace std;
 
 int main(int argc, char* argv[]) {
+    // Name of the text file to use
     string fileName = argv[1];
+    // Start the HashTable program
     HashTable ht (fileName);
     ht.run();
     return 0;
